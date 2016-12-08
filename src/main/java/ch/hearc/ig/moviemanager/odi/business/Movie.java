de8af2228d96e84c59dad35d7d9e228fd7cd247b
steven.habegger <steven.habegger@he-arc.ch>
2016-12-08 09:00:40 +0100

    Création du constructeur/getter/setter de la classe film

--- src/main/java/ch/hearc/ig/moviemanager/odi/business/Movie.java
+++ src/main/java/ch/hearc/ig/moviemanager/odi/business/Movie.java
@@ -14,4 +14,64 @@ public class Movie {
     private String producer;
     HashMap<Long, Person> persons; 
     
+    /**
+     * Constructeur de film
+     *
+     * @param id Identifiant du film
+     * @param name Titre du film
+     * @param producer Nom du producteur
+     */
+    public Movie(long id, String name, String producer) {
+        this.id = id;
+        this.name = name;
+        this.producer = producer;
+        this.persons = new HashMap<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    /**
+     * Renvoie les personnes ayant vu le film
+     *
+     * @return Liste de personnes
+     */
+    public HashMap<Long, Person> getPersons() {
+        return persons;
+    }
+
+    /**
+     * Modifie les personnes ayant vu le film
+     *
+     * @param persons Listes des nouvelles personnes
+     */
+    public void setPersons(HashMap<Long, Person> persons) {
+        this.persons = persons;
+    }
+
+    @Override
+    public String toString() {
+        return this.id + " - " + this.name;
+    }
 }
