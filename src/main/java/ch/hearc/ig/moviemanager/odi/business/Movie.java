b1cb31e9525b43f8b473c96260045b63c074c01a
steven.habegger <steven.habegger@he-arc.ch>
2016-12-08 08:54:15 +0100

    Création de la classe film

---
+++ src/main/java/ch/hearc/ig/moviemanager/odi/business/Movie.java
@@ -0,0 +1,18 @@
+package ch.hearc.ig.moviemanager.odi.business;
+
+import java.util.HashMap;
+
+/**
+ *Classe métier de film.
+ * 
+ * @author steven.habegger
+ */
+public class Movie {
+    
+    private long id;
+    private String name;
+    private String producer;
+// En attente de la création de la classe "person"    
+//    HashMap<Long, Person> persons; 
+    
+}
